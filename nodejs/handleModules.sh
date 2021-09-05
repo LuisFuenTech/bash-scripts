cc265168c2977ef0bcf31eae4493bb4c8b7a8942
luisfuentech <luisfuentes.au@gmail.com>
2021-09-05 03:22:58 -0500

    Feat: Update logs to be nested

--- nodejs/handleModules.sh
+++ nodejs/handleModules.sh
@@ -43,31 +43,31 @@ function checkError() {
 }
 
 function removeNpmModules() {
-    echo "Init NPM dependencies removing...⏳\n" && sleep 0.5
-    echo "Moving to $PWD... 🚀\n" && sleep 0.5
+    echo "Init NPM dependencies removing...⏳" && sleep 0.5
+    echo "|----->\tMoving to $PWD... 🚀" && sleep 0.5
     cd $PWD || ERROR=true
     checkError
 
     if [[ -f $PACKAGE_LOCK ]]; then
         ACTION_FLAG=true
-        echo "Removing '$PACKAGE_LOCK' file...⚙️" && sleep 0.5
+        echo "|----->\tRemoving '$PACKAGE_LOCK' file...⚙️" && sleep 0.5
         rm $PACKAGE_LOCK || ERROR=true
         checkError
 
-        echo "'$PACKAGE_LOCK' file removed! ✅\n" && sleep 0.5
+        echo "\t|_____'$PACKAGE_LOCK' file removed! ✅" && sleep 0.5
     fi
 
     if [[ -d $NODE_MODULES ]]; then
         ACTION_FLAG=true
-        echo "Removing '$NODE_MODULES' directory...⚙️" && sleep 0.5
+        echo "|----->\tRemoving '$NODE_MODULES' directory...⚙️" && sleep 0.5
         rm -rf $NODE_MODULES || ERROR=true
         checkError
 
-        echo "'$NODE_MODULES' directory removed! ✅\n" && sleep 0.5
+        echo "\t|_____'$NODE_MODULES' directory removed! ✅" && sleep 0.5
     fi
 
     if [[ $ACTION_FLAG == false ]]; then
-        echo "There's nothing to remove! 🗑\n" && sleep 0.5
+        echo "\nThere's nothing to remove! 🗑\n" && sleep 0.5
     else
         echo "NPM dependencies were removed! ✅\n" && sleep 0.5
     fi
@@ -75,9 +75,9 @@ function removeNpmModules() {
 }
 
 function installNpmModules() {
-    echo "Init NPM dependencies installing...⏳\n" && sleep 0.5
+    echo "Init NPM dependencies installing...⏳" && sleep 0.5
     if [[ -f $PACKAGE ]]; then
-        echo "Installing NPM dependencies...⚙️\n" && sleep 0.5
+        echo "|----->\tInstalling NPM dependencies...⚙️" && sleep 0.5
 
         if [[ $IS_INSTALL_OPTIONAL == true ]]; then
             npm i $ENTRY_OPTIONAL_COMMAND || NPM_ERROR=true
@@ -111,7 +111,6 @@ then
     removeNpmModules
     installNpmModules
 else
-
     echo "The command '$ENTRY_COMMAND' isn't correct ❌"
     echo "$COMMAND_MAN"
 fi
