0e51bad487ceb72265d01d683bb076f02cc9d8ac
luisfuentech <luisfuentes.au@gmail.com>
2022-01-14 20:35:14 -0600

    Perf: Update script names

--- git-credentials/token.sh
+++ git-credentials/token.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-### TODO: Check if the remote have username and token added by regex, then validate it
 username="$1"
 token="$2"
 remote=$(git remote -v | grep fetch)
 GIT_ERROR=false
 ERROR=false
 REMOTE_GITHUB_PATTERN='^(https|http):\/\/github\.com\/(.*)\.git$'
-REMOTE_GITHUB_TOKEN_PATTERN='^(https|http):\/\/[0-9a-zA-Z_]+:[0-9a-zA-Z_]+@github\.com\/(.*)\.git$'
+REMOTE_GITHUB_TOKEN_PATTERN='^(https|http):\/\/[0-9a-zA-Z_]+(:[0-9a-zA-Z_]+)?@github\.com\/(.*)\.git$'
 url=""
 protocolHttp=""
 githubProject=""
@@ -23,19 +22,21 @@ if [[ $# -eq 0 ]] || [[ -z $@ ]]; then
     exit 1
 fi
 
+echo "Moving to $PWD... 🚀" && sleep 0.5
+cd $PWD || ERROR=true
+checkError
+
 echo "Reading local origin remote... 👀"
 values=(${remote/// })
 url=${values[1]}
 
 if [[ $url =~ $REMOTE_GITHUB_PATTERN ]]; then
-    echo "${BASH_REMATCH[0]}"
     protocolHttp=${BASH_REMATCH[1]}
     githubProject=${BASH_REMATCH[2]}
 
 elif [[ $url =~ $REMOTE_GITHUB_TOKEN_PATTERN ]]; then
-    echo "${BASH_REMATCH[0]}"
     protocolHttp=${BASH_REMATCH[1]}
-    githubProject=${BASH_REMATCH[2]}
+    githubProject=${BASH_REMATCH[3]}
 else
     echo "💥 Invalid remote url -> '$url' 💥"
     exit 1
@@ -46,7 +47,7 @@ newOriginRemote="$protocolHttp://$username:$token@github.com/$githubProject.git"
 
 echo "Updating local origin remote... 📝"
 git remote set-url origin $newOriginRemote || GIT_ERROR=true
-git config --local credential.helper cache || GIT_ERROR=true
+git config --local credential.helper cache --replace-all || GIT_ERROR=true
 checkError
 
 echo "Origin remote updated ✅ -> $newOriginRemote"
